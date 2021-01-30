55c3b7e50866b752f5acb01939ce4a91dc8a6816
Antonio Scardovi <antonio.scardovi@gmail.com>
2021-01-30 11:12:17 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import quotes from './quotes'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const { motion, useMotionValue } = Motion;
+// import { motion } from "https://cdn.skypack.dev/framer-motion"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0,
+      quotes: quotes,
+      test: 'Random Quote'
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  
+  handleClick() {
+    this.setState((state) => {
+      let newIndex = Math.floor(Math.random() * 10);
+      while(this.state.index === newIndex) {
+        newIndex = Math.floor(Math.random() * 10)
+      }
+      return { index: newIndex }
+    })
+  }
+  
+  render() {
+    return(
+      <div id="container">
+        <div id="quote-box">
+          <h1 id="text"><i class="fas fa-quote-left"></i> {this.state.quotes[this.state.index].quote}</h1>
+          <p id="author">- {this.state.quotes[this.state.index].author}</p>
+          <div id="actions">
+          <a href="twitter.com/intent/tweet" id="tweet-quote"><i class="icon fab fa-twitter fa-lg"></i></a>
+          <button id="new-quote" onClick={this.handleClick}>New quote</button>
+          </div>
+            </div>
+          
+        <p>by <a href="https://github.com/antonioscardovi" id='git-link' rel="noreferrer" target="_blank">antonioscardovi</a></p>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<QuoteBox />, document.querySelector("#app"))
